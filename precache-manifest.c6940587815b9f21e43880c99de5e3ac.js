be481345483e28efe75be35570d16c67fb1ede7e
Edmundo Biglia <edmundobiglia@gmail.com>
2020-11-02 01:13:04 -0300

    Updates

---
+++ precache-manifest.c6940587815b9f21e43880c99de5e3ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeee845dad6d45224682144d99ea4be7",
+    "url": "/brev/index.html"
+  },
+  {
+    "revision": "845d575bcfc91a9631ce",
+    "url": "/brev/static/css/main.40f8df7f.chunk.css"
+  },
+  {
+    "revision": "cd92c2fe7602e2a8ef28",
+    "url": "/brev/static/js/2.320b6a70.chunk.js"
+  },
+  {
+    "revision": "8e8901d4352116ce093d3295dc72f65d",
+    "url": "/brev/static/js/2.320b6a70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845d575bcfc91a9631ce",
+    "url": "/brev/static/js/main.d6392b49.chunk.js"
+  },
+  {
+    "revision": "f5b839eeddddae6315d3",
+    "url": "/brev/static/js/runtime-main.96db0bab.js"
+  },
+  {
+    "revision": "e25121f09746b515b1ac875559aeaad4",
+    "url": "/brev/static/media/brev_logo.e25121f0.svg"
+  }
+]);
